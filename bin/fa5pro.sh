e14c066e67859a071c310b3f6768315123577072
Alexander Lyon <arlyon@me.com>
2020-05-29 11:04:53 +0100

    Add custom prompt and allow silent mode (for CI)

--- bin/fa5pro.sh
+++ bin/fa5pro.sh
@@ -1,11 +1,36 @@
 #!/bin/sh
 
-if [ $(npm config get @fortawesome:registry) == "undefined" ]
+silent=false
+prompt=""
+
+usage() { echo "$0 usage:" && grep " .)\ #" $0; exit 0; }
+
+while getopts ":sp:" arg; do
+    case ${arg} in
+        s) # Whether to fail silently on font issues (for CI).
+            silent=true
+            ;;
+        p) # Custom prompt to show to the user.
+            prompt=" (${OPTARG})"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ $(npm config get @fortawesome:registry) == "undefined" ] && [ $silent == false ]
 then
-  echo >&2 "Fonts not set up! Please enter your FA5 Pro Key:"
-  read -p "Key: " fa5_key
+  echo >&2 "Fonts not set up! Please enter your FA5 Pro Key"
+  read -p "Key${prompt}: " fa5_key
+  if [ -z ${fa5_key} ]
+  then
+    echo "No key set. Skipping."
+    exit 0
+  fi
   npm config set "@fortawesome:registry" https://npm.fontawesome.com/
-  npm config set "//npm.fontawesome.com/:_authToken" $fa5_key 
+  npm config set "//npm.fontawesome.com/:_authToken" $fa5_key
 fi
 
 echo >&2 "Found @fortawesome npm configuration. Installing fonts"
