a2d8df8dc4d9add9717eb9f05494522702ae6dfa
Alexander Lyon <arlyon@me.com>
2020-05-29 10:30:28 +0100

    Add key prompt if not set

--- bin/fa5pro.sh
+++ bin/fa5pro.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
-if [ $(npm config get @fortawesome:registry) != "undefined" ]
+if [ $(npm config get @fortawesome:registry) == "undefined" ]
 then
-  echo >&2 "Found @fortawesome npm configuration. Installing fonts"
-  echo >&2 "Creating temporary folder"
-  TEMP_DIR=$(mktemp -d -t rnvi)
-  echo >&2 "Created folder $TEMP_DIR"
-  pushd ${TEMP_DIR}
-
-  echo >&2 "Downloading Font Awesome 5 Pro"
-
-  ARCHIVE=$(npm pack @fortawesome/fontawesome-pro --silent) || { echo >&2 "Unable to download fonts. Make sure your @fortawesome scope is configured correctly"; exit 0; }
-  tar -xzf ${ARCHIVE}
-  mv package pro
-
-  popd
-
-  echo >&2 "Copying font files"
-
-  cp ${TEMP_DIR}/pro/webfonts/fa-brands-400.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Brands.ttf
-  cp ${TEMP_DIR}/pro/webfonts/fa-light-300.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Light.ttf
-  cp ${TEMP_DIR}/pro/webfonts/fa-regular-400.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Regular.ttf
-  cp ${TEMP_DIR}/pro/webfonts/fa-solid-900.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Solid.ttf
-
-  echo >&2 "Done"
+  echo >&2 "Fonts not set up! Please enter your FA5 Pro Key:"
+  read -p "Key: " fa5_key
+  npm config set "@fortawesome:registry" https://npm.fontawesome.com/
+  npm config set "//npm.fontawesome.com/:_authToken" $fa5_key 
 fi
+
+echo >&2 "Found @fortawesome npm configuration. Installing fonts"
+echo >&2 "Creating temporary folder"
+TEMP_DIR=$(mktemp -d -t rnvi)
+echo >&2 "Created folder $TEMP_DIR"
+pushd ${TEMP_DIR}
+
+echo >&2 "Downloading Font Awesome 5 Pro"
+
+ARCHIVE=$(npm pack @fortawesome/fontawesome-pro --silent) || { echo >&2 "Unable to download fonts. Make sure your @fortawesome scope is configured correctly"; exit 0; }
+tar -xzf ${ARCHIVE}
+mv package pro
+
+popd
+
+echo >&2 "Copying font files"
+
+cp ${TEMP_DIR}/pro/webfonts/fa-brands-400.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Brands.ttf
+cp ${TEMP_DIR}/pro/webfonts/fa-light-300.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Light.ttf
+cp ${TEMP_DIR}/pro/webfonts/fa-regular-400.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Regular.ttf
+cp ${TEMP_DIR}/pro/webfonts/fa-solid-900.ttf ./build/vendor/react-native-vector-icons/Fonts/FontAwesome5_Pro_Solid.ttf
+
+echo >&2 "Done"
+
